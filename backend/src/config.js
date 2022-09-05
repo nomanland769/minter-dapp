36d6bfb6072989fa049e17468535e48de8b04b6f
Daniel Kua <danielkua888@live.com>
2022-09-05 01:34:07 -0700

    change dates

--- backend/src/config.js
+++ backend/src/config.js
@@ -65,7 +65,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-10-10T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-10-10T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xCA956E0e5b795646F7A5AE32Db7fDAe042f20E15"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
